15175f7df64346898339c9ccc79a5530e6958acc
simonprod <xeywar@gmail.com>
2021-11-05 20:22:55 +0300

    fix description

--- scripts/issue.bash
+++ scripts/issue.bash
@@ -8,8 +8,10 @@ prevTag=$(git tag --sort=-creatordate | head -2 | tail -1)
 summary="release $tag - by Simon"
 author=$(git show ${tag} | grep Author: | head -1)
 date=$(git show ${tag} | grep Date: | head -1)
-changelog=$(git log --pretty="%H %s (%an)\n" ${prevTag}...${tag} )
-description="Released by ${author}\n${date}\n\n${changelog}"
+changelog=$(git log --pretty="%H %s (%an) <br/>" ${prevTag}...${tag} | tr -s "\n" " ")
+description="Released by ${author}\n${date}\n\n Changelog:\n<#${changelog}#>"
+
+echo $description
 
 curl -X POST ${API_URL} \
   -H "Content-Type: application/json", \
