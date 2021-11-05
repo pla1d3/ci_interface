3378dc19f7e8e79dd18b314580238df464b7b925
simonprod <xeywar@gmail.com>
2021-11-05 19:50:49 +0300

    fix: fetch-depth

--- scripts/issue.bash
+++ scripts/issue.bash
@@ -2,16 +2,14 @@
 
 API_URL="https://api.tracker.yandex.net/v2/issues"
 
-tag=$(git tag | sort -r | head -n1)
-prevTag=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
+tag=$(git tag --sort=-creatordate | head -1)
+prevTag=$(git tag --sort=-creatordate | head -2 | tail -1)
 
 summary="release $tag - by Simon"
 author=$(git show ${tag} | grep Author: | head -1)
 date=$(git show ${tag} | grep Date: | head -1)
-changelog=$(git log ${prevTag}...${tag} --oneline -1)
+changelog=$(git log --pretty="%H %s (%an)\n" ${prevTag}...${tag} )
 description="Released by ${author}\n${date}\n\n${changelog}"
-
-echo $description
 
 curl -X POST ${API_URL} \
   -H "Content-Type: application/json", \
